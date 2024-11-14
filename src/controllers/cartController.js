6ff075caee50aad94f3887f92c20c4da2cbb3ca0
nidheeshentri <nidheesh.b@entri.me>
2024-11-14 20:49:31 +0530

    cart count

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,7 +1,37 @@
 const CartModel = require("../models/cartModel")
 
-const getCartItems = (req, res) => {
-    res.send("All cart items")
+const getCartItems = async (req, res) => {
+    let user = req.user
+    let cartItems = await CartModel.find({user: user._id}).populate('product')
+    console.log(cartItems)
+    res.status(200).json({cartItems: cartItems})
+    // res.send("All cart items")
+}
+
+const updateCartItemCount = async (req, res) => {
+    let count = req.body.count
+    let cartItemId = req.body.cartItemId
+
+    if (count>0){
+        let cartItem = await CartModel.findById(cartItemId)
+        cartItem.count = count
+        cartItem.save()
+        .then(cart => {
+            res.status(200).json({cartItem: cart})
+        })
+        .catch(err =>{
+            res.status(400).json({message: "Bad request. Try again", error: err})
+        })
+    }
+    else{
+        CartModel.findOneAndDelete({ _id: cartItemId })
+        .then(()=>{
+            res.status(200).json({message: "Deleted successfully"})
+        })
+        .catch(()=>{
+            res.status(400).json({message: "Bad request"})
+        })
+    }
 }
 
 const AddToCart = (req, res) => {
@@ -15,4 +45,4 @@ const AddToCart = (req, res) => {
     })
 }
 
-module.exports = {getCartItems, AddToCart}
+module.exports = {getCartItems, AddToCart, updateCartItemCount}
--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -1,9 +1,10 @@
 const express = require("express")
-const { getCartItems, AddToCart } = require("../controllers/cartController")
+const { getCartItems, AddToCart, updateCartItemCount } = require("../controllers/cartController")
 const authenticateUser = require("../middlewares/authenticationMiddleware")
 const router = express.Router()
 
-router.get("/", getCartItems)
+router.get("/", authenticateUser, getCartItems)
 router.post("/", authenticateUser, AddToCart)
+router.post("/update-count", authenticateUser, updateCartItemCount)
 
 module.exports = router
